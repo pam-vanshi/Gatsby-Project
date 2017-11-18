500fe7bbbdea63481eaa0bb9b917f6b437d3dcff
Pramudit <spramudit@gmail.com>
2017-11-18 07:59:20 +0530

    Second commit

---
+++ src/pages/counter.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+class Counter extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Counter</h1>
+        <p>current count: 0</p>
+        <button>plus</button>
+        <button>minus</button>
+      </div>
+    )
+  }
+}
+export default Counter
